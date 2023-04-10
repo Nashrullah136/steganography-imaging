baeb1684611945c91c4e8aa584730e7ac38abbf0
Nashrullah136 <muhammad.118140136@student.itera.ac.id>
2023-04-10 21:07:39 +0700

    modify app entry

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-import tkinter as tk
-from src.gui.app import App
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from src.gui.pages.image.insert_form import InsertForm
+from src.gui.pages.image.extract_form import ExtractForm
+from src.gui.pages.main_page import MainPage
+from src.helper.Navigation import Navigation
+
+def main():
+   app = QApplication(sys.argv)
+   nav = Navigation()
+   insert_form = InsertForm(nav, nav)
+   extract_form = ExtractForm(nav, nav)
+   main_page = MainPage(nav, nav)
+   nav.addWidget("main_page", main_page)
+   nav.addWidget("insert_form", insert_form)
+   nav.addWidget("extract_form", extract_form)
+   nav.go_to("main_page")
+   nav.show()
+   nav.adjustSize()
+   sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    app = App()
-    app.geometry('{}x{}'.format(700, 500))
-    app.mainloop()
+   main()

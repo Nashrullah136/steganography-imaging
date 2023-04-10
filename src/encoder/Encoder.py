7b2413b895f0bcc856f0d5b47feaaf5dc3859511
Nashrullah136 <muhammad.118140136@student.itera.ac.id>
2023-04-10 12:27:07 +0700

    Add Encoder

---
+++ src/encoder/Encoder.py
@@ -0,0 +1,8 @@
+from src.helper.BinaryDigitArray import BinaryDigitArray
+
+class Encoder:
+    def encode(self, msg: BinaryDigitArray) -> BinaryDigitArray:
+        pass
+    def decode(self, msg: BinaryDigitArray) -> BinaryDigitArray:
+        pass
+    
